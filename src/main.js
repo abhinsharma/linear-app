d5e7e241b4087b71d6d8ae8cfbdca2afdfc63b28
Abhin Sharma <abhin_sh@yahoo.com>
2026-03-07 21:21:47 -0800

    feat: add compact release operator hint in footer

--- src/main.js
+++ src/main.js
@@ -146,6 +146,7 @@ document.querySelector('#app').innerHTML = `
             Reviewer: release-reviewers@acme.dev
           </a>
         </p>
+        <p class="footer-operator-hint">Operator hint: Run go/no-go after checklist reaches 10/10.</p>
         <p class="footer-release-note">Release date: March 4, 2026</p>
       </div>
     </footer>
